01876677370f59c7196f0dc6eca95ec9b7ec2250
iamanggi <anggipuspita452@gmail.com>
2023-03-06 11:46:52 +0700

    first commit

---
+++ contoh.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hai!" << endl;
+    cout << "aku anggi hihi" << endl;
+
+    return 0;
+}
---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //begin 
+    // numeric Luas, p, l 
+    double Luas, p, l;
+    // display 'Masukan panjangnya = '
+    cout << "Masukan panjangnya = ";
+    // accept p 
+    cin >> p;
+    // display 'masukan lebarnya = ' 
+    cout << "masukan lebarnya = ";
+    // accept l
+    cin >> l;
+    // compute Luas = p*l
+    Luas = p*l;
+    // display 'Luasnya = ' + Luas 
+    cout << "luasnya = " << Luas; 
+    // end 
+}
